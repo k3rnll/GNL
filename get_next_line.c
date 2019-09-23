b6c57c85746ed042e04817da480e5c2f3b63ef34
k3rnll <kanaikin.greg@gmail.com>
2019-09-23 20:59:50 +0300

    .

--- get_next_line.c
+++ get_next_line.c
@@ -15,18 +15,20 @@
 int		get_next_line(const int fd, char **line)
 {
 	char 	buf[BUFF_SIZE];
+	static int	*arr[20];
+	int		ret;
 	char 	*tmp;
 	char	*new;
 	int		var;
 	size_t	len;
 
 	new = ft_strnew(0);
-	len = ft_strlen(new);
+	len = 0;
 	while (ret = read(fd, buf, BUFF_SIZE))
 	{
-		new = ft_strsub(buf, 0, ret);
-		f_lend(&new);
-		tmp = ft_strjoin(tmp, buf);
+		if
+		tmp = ft_strsub(buf, 0, ret);
+		new = ft_strjoin(new, tmp);
 		line = &tmp;
 		return (1);
 	}
